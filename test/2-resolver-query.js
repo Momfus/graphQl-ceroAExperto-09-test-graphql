ad14e065a510658b6d6e6956ffa2b03b8bfc2a2b
Momfus <momfus@outlook.com>
2020-12-14 20:24:19 -0300

    Agregado archivos y carpetas base - Preparativos para testear resolver - clase 345 de mock

---
+++ test/2-resolver-query.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const EasyGraphQLTester = require('easygraphql-tester');
+const fs = require('fs');
+const path = require('path');
+
+const apiSchema = fs.readFileSync(
+  path.join(__dirname, './../src/schema', 'schema.graphql'),
+  'utf8'
+);
+
+describe("Testing Resolvers - Type Root - Query", () => {
+
+    let tester;
+    before( function() {
+
+        tester = new EasyGraphQLTester( apiSchema );
+
+    });
+
+    it("Comprobar que 'hello' devuelve correcto", async() => { // Necesita ser asíncrona por el tipo de test a realizar
+
+    });
+
+});
